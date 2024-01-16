10e46ccf5cddeff03e6fff97fbdfe5125359730c
alexeosss <oleksii.melnychenko.kpi@gmail.com>
2024-01-16 12:54:39 +0200

    alexm-1254

---
+++ app/src/main/java/com/missclick/alies/ui/components/SmallTeamCard.kt
@@ -0,0 +1,16 @@
+package com.missclick.alies.ui.components
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SmallTeamCard(teamImage: String, teamName: String){
+
+    Card(modifier = Modifier.size(128.dp, 48.dp)) {
+
+    }
+
+}
--- app/src/main/java/com/missclick/alies/ui/theme/Type.kt
+++ app/src/main/java/com/missclick/alies/ui/theme/Type.kt
@@ -3,20 +3,32 @@ package com.missclick.alies.ui.theme
 
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.missclick.alies.R
 
 
 data class Typography(
-    val headerText : TextStyle,
+    val headerTextBold : TextStyle,
+    val headerTextThin : TextStyle,
 )
 
 
 val typography = Typography(
-    headerText = TextStyle(
+    headerTextBold = TextStyle(
         fontFamily = FontFamily(Font(R.font.playbold)),
         fontSize = 36.sp,
         fontWeight = FontWeight.Bold
-    )
+    ),
+    headerTextThin = TextStyle(
+        fontFamily = FontFamily(Font(R.font.playbold)),
+        fontSize = 36.sp,
+        fontWeight = FontWeight.Normal
+    ),
+
+
 )
 
 val LocalTypographyProvider = staticCompositionLocalOf<Typography> {
