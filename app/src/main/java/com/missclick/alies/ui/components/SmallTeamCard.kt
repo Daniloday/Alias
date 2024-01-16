2eb0832ef08390757125643191fd7f9f33d1e79a
alexeosss <oleksii.melnychenko.kpi@gmail.com>
2024-01-16 13:11:15 +0200

    alexm-1310

--- app/src/main/java/com/missclick/alies/ui/components/SmallTeamCard.kt
+++ app/src/main/java/com/missclick/alies/ui/components/SmallTeamCard.kt
@@ -1,6 +1,7 @@
 package com.missclick.alies.ui.components
 
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -9,7 +10,7 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun SmallTeamCard(teamImage: String, teamName: String){
 
-    Card(modifier = Modifier.size(128.dp, 48.dp)) {
+    Card(modifier = Modifier.size(128.dp, 48.dp), shape = RoundedCornerShape(20.dp)) {
 
     }
 
