d90fbe9d2d8c9b094c8e66af60f7d6505d7ea598
incarnadine24 <wenqzz.123@gmail.com>
2024-01-16 13:03:19 +0200

    wenq

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.missclick.alies"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.missclick.alies"
--- app/src/main/java/com/missclick/alies/ui/theme/Type.kt
+++ app/src/main/java/com/missclick/alies/ui/theme/Type.kt
@@ -3,7 +3,11 @@ package com.missclick.alies.ui.theme
 
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.missclick.alies.R
 
 
 data class Typography(
