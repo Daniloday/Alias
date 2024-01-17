90018c28fe661c8755228be46ca2ea14a0054728
daniloday <danil.vladyka1@gmail.com>
2024-01-17 13:07:15 +0200

    fix

--- app/src/main/java/com/missclick/alies/data/repository/team/LocalTeams.kt
+++ app/src/main/java/com/missclick/alies/data/repository/team/LocalTeams.kt
@@ -10,10 +10,10 @@ class LocalTeams(private val context : Context) : ILocalTeams {
     override fun getAll(): List<Team> {
         return listOf(
             Team(name = context.getString(R.string.lions), image = R.drawable.lion),
-            Team(name = context.getString(R.string.agama), image = R.drawable.agama),
+            Team(name = context.getString(R.string.agamas), image = R.drawable.agama),
             Team(name = context.getString(R.string.capybaras), image = R.drawable.capybara),
             Team(name = context.getString(R.string.giraffes), image = R.drawable.giraffe),
-            Team(name = context.getString(R.string.rabbit), image = R.drawable.hare),
+            Team(name = context.getString(R.string.rabbits), image = R.drawable.hare),
             Team(name = context.getString(R.string.hedgehogs), image = R.drawable.hedgehog),
             Team(name = context.getString(R.string.owls), image = R.drawable.owl),
             Team(name = context.getString(R.string.rhinos), image = R.drawable.rhino),
